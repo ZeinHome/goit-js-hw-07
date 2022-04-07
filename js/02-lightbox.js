29d62fc52ce701722dfbbc135eff55a7e0c87858
ZeinHome <zein3288@gmail.com>
2022-04-07 15:32:35 +0300

    ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function makeGallery(galleryArray) {
+    return galleryArray.map(({ preview, original, description }) => {
+        return `<li>
+          <a class="gallery__item" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}" 
+          alt="${description}"
+          style="display: block"
+          />
+        </a>
+      </li>`;
+    }).join('');
+}
+galleryEl.insertAdjacentHTML('beforeend', makeGallery(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
